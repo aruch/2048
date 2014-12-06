706c82b881daf983e23079301998133a53d9b7ae
Alex Ruch <ahruch@gmail.com>
2014-12-05 22:42:13 -0700

    added heuristics to board

--- py2048/board.py
+++ py2048/board.py
@@ -1,12 +1,14 @@
 """Define the class representing the game 2048"""
 
 import numpy as np
+import copy
 
 GRID_SIZE = (4,4)
 PROB_2 = 0.9
+DIRS = ["left", "right", "up", "down"]
 
 def pos(i, j, flip=False):
-    """accessory function to access by column and row instead of row and column"""
+    """Accessory function to access by column and row instead of row and column"""
     if flip:
         return (j, i)
     else:
@@ -32,6 +34,10 @@ class Board:
         array representing the current state of the game
     prob_2 : numeric in [0,2]
         Initialized probability of generating a 2
+    game_over : bool
+        Represents whether the game is over
+    score : int
+        Current score of the game
     """
     def __init__(self, grid_size=GRID_SIZE, prob_2=PROB_2):
         """Initialize board by creating an array of shape grid_size.
@@ -39,17 +45,23 @@ class Board:
         grid_size is assumed to be the same in both dimensions"""
         self.grid = np.ones(grid_size)
         self.prob_2 = prob_2
+        self.game_over = False
+        self.score = 0
 
-        
-    def move(self, d):
-        """Moves the tiles in the direction specified
+    def move(self, d, check_only=False):
+        """Moves the tiles in the direction specified, or checks if a
+        move could be made in that direction.
 
-        Does not check the legitimacy of move for speed purposes
+        Does not check the legitimacy of move for speed purposes.
+        Increments the score when two tiles are merged.
 
         Parameters
         ----------
         d : str
-            The direction to move the tiles on the grid in ["left", "right", "up", "down"]
+            The direction to move the tiles on the grid
+            should be in ["left", "right", "up", "down"]
+        check_only : bool
+            Method returns True if a move could be made in the direction d
         """
 
         if d == "left" :
@@ -77,23 +89,29 @@ class Board:
             first_empty = start
             compressed = True
             # traverse the row/column in the opposite direction of the move
-            # move tiles to the first empty space seen, or if the last move didn't
-            # result in a merge of two tiles check if the first non-empty space contains
-            # a tile with the same value as the one being moved.  If that is the case
-            # combine the two tiles together into one.
+            # move tiles to the first empty space seen, or if the last move
+            # didn't result in a merge of two tiles check if the first
+            # non-empty space contains a tile with the same value as the one
+            # being moved.  If that is the case combine the two tiles together
+            # into one.
             #
-            # When moving "up" or "down" flip the indices so that access is done (column, row)
-            # instead of (row, column)
+            # When moving "up" or "down" flip the indices so that access is
+            # done (column, row) instead of (row, column)
             for j in range(start, stop, inc):
                 if self.grid[pos(i, j, flip)] != 1:
                     if (not compressed and
                         self.grid[pos(i, first_empty - inc, flip)] ==
                         self.grid[pos(i, j, flip)]):
 
+                        if check_only:
+                            return True
                         self.grid[pos(i, first_empty - inc, flip)] *= 2
+                        self.score += self.grid[pos(i, first_empty - inc, flip)]
                         self.grid[pos(i, j, flip)] = 1
                         compressed = True
                     elif j != first_empty:
+                        if check_only:
+                            return True
                         self.grid[pos(i, first_empty, flip)] = self.grid[pos(i, j, flip)]
                         self.grid[pos(i, j, flip)] = 1
                         first_empty += inc
@@ -111,3 +129,119 @@ class Board:
         """Returns a list of tuples of the positions of the empty tiles"""
 
         return list(zip(*np.where(self.grid == 1)))
+
+    def add_random_tile(self):
+        """Adds a tile to a random empty spot on the grid.  The value
+        of the tile is randomly determined."""
+        tile_value = 2 if np.random.random() < self.prob_2 else 4
+
+        empty_tiles = self.empty_tiles()
+        self.grid[empty_tiles[np.random.choice(len(empty_tiles))]] = tile_value
+
+    def possible_moves(self):
+        """Returns a list of the possible move directions"""
+        moves = []
+        for d in DIRS:
+            if self.move(d, check_only=True):
+                moves.append(d)
+
+        return moves
+
+    def check_game_over(self):
+        return self.n_empty_tiles() == 0 and self.possible_moves() == []
+
+    def turn(self, d):
+        """Taking a turn in the game consists of making a move followed by
+        adding a tile to the board.
+
+        Assumes game is not over to start and
+        checks if the game is over at the end."""
+        self.move(d)
+        self.add_random_tile()
+        self.game_over = self.check_game_over()
+
+    def n_merges_available(self):
+        """Count the number of tiles that can be merged
+
+        Returns : int"""
+        saved_grid = self.grid.copy()
+        saved_score = self.score
+        init_empty = self.n_empty_tiles()
+        self.move("left")
+        horizontal_merges = self.n_empty_tiles() - init_empty 
+        self.grid = saved_grid.copy()
+        self.move("up")
+        vertical_merges = self.n_empty_tiles() - init_empty 
+        self.grid = saved_grid.copy()
+
+        return horizontal_merges + vertical_merges
+
+    def smoothness(self):
+        """Smoothness is a measure of how similar adjacent tiles are
+
+        Calculates the log difference of adjacent tiles.  Adjacent doesn't
+        necessarily mean within one space. Totals the horizontal and
+        vertical directions.
+
+        Returns : float
+        """
+
+        smoothness = 0
+
+        log_grid = np.log(self.grid)
+
+        for i in range(self.grid.shape[0]):
+            previous_tile = 0
+            for j in range(self.grid.shape[1]):
+                if log_grid[i, j] != 0:
+                    if previous_tile != 0:
+                        smoothness += np.abs(previous_tile - log_grid[i, j])
+                    previous_tile = log_grid[i, j]
+
+        for i in range(self.grid.shape[1]):
+            previous_tile = 0
+            for j in range(self.grid.shape[0]):
+                if log_grid[j, i] != 0:
+                    if previous_tile != 0:
+                        smoothness += np.abs(previous_tile - log_grid[j, i])
+                    previous_tile = log_grid[j, i]
+
+        return smoothness
+
+    def n_out_of_order(self):
+        """Measure of the monotonicity of the board
+
+        Sums the swaps necessary to put the board in order.
+        Runs a vertical and horizontal pass picking the minimum
+        (out of either left and right or up and down) for each.
+
+        Returns : int
+        """
+        l_out_of_order = 0
+        r_out_of_order = 0
+        for i in range(self.grid.shape[0]):
+            for j in range(self.grid.shape[1]-1):
+                cur_val = self.grid[i, j]
+                if cur_val != 1:
+                    cur_slice = self.grid[i, (j+1):]
+                    l_out_of_order += np.sum((cur_slice > cur_val) &
+                                             (cur_slice != 1))
+                    r_out_of_order += np.sum((cur_slice < cur_val) &
+                                             (cur_slice != 1))
+
+        h_out_of_order = min(l_out_of_order, r_out_of_order)
+
+        u_out_of_order = 0
+        d_out_of_order = 0
+        for i in range(self.grid.shape[1]):
+            for j in range(self.grid.shape[0]-1):
+                cur_val = self.grid[j, i]
+                if cur_val != 1:
+                    cur_slice = self.grid[(j+1):, i]
+                    u_out_of_order += np.sum((cur_slice > cur_val) &
+                                             (cur_slice != 1))
+                    d_out_of_order += np.sum((cur_slice < cur_val) &
+                                             (cur_slice != 1))
+        v_out_of_order = min(u_out_of_order, d_out_of_order)
+
+        return h_out_of_order + v_out_of_order
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,8 +1,5 @@
 import numpy as np
-from py2048.board import Board
-
-GRID_SIZE = (4,4)
-PROB_2 = 0.9
+from py2048.board import Board, GRID_SIZE, PROB_2, DIRS
 
 def test_move_left():
     b = Board(GRID_SIZE, PROB_2)
@@ -96,6 +93,33 @@ def test_move_down():
                                       [4, 4, 2, 4],
                                       [4, 128, 128, 4]]))
 
+def test_score_increment_on_move():
+    b = Board(GRID_SIZE, PROB_2)
+    # no merges
+    b.score = 0
+    b.grid = np.array([[2, 4, 1, 1],
+                       [1, 2, 1, 1],
+                       [1, 1, 2, 8],
+                       [1, 1, 1, 1]])
+    b.move("down")
+    assert b.score == 0
+
+    # 1 and 2 merges
+    b.score = 0
+    b.grid = np.array([[2, 2, 4, 2],
+                       [2, 2, 2, 2],
+                       [4, 64, 1, 2],
+                       [1, 64, 128, 2]])
+    b.move("down")
+    assert b.score == 4 + 128 + 4 + 4 + 4
+    b.score = 0
+    b.grid = np.array([[2, 2, 4, 2],
+                       [2, 2, 2, 2],
+                       [4, 64, 1, 2],
+                       [1, 64, 128, 2]])
+    b.move("right")
+    assert b.score == 4 + 4 + 4
+
 def test_n_empty_tiles():
     b = Board(GRID_SIZE, PROB_2)
     b.grid = np.array([[1, 1, 1, 1],
@@ -147,3 +171,211 @@ def test_empty_tiles():
                        [2, 2, 2, 2],
                        [2, 2, 2, 2]])
     assert b.empty_tiles() == []
+
+def test_add_random_tile():
+    # don't test this method currently.
+    # would need to be tightly coupled to implementation details
+    pass
+
+def test_turn():
+    # don't test this method currently.
+    # would need to be tightly coupled to implementation details
+    pass
+
+def test_possible_moves():
+    b = Board()
+    b.grid = np.array([[2, 2, 2, 2],
+                       [2, 2, 2, 2],
+                       [2, 2, 2, 2],
+                       [2, 2, 2, 2]])
+
+    assert b.possible_moves() == DIRS
+
+    b.grid = np.array([[1, 1, 1, 2],
+                       [1, 1, 1, 2],
+                       [1, 1, 1, 2],
+                       [1, 1, 1, 2]])
+
+    assert b.possible_moves() == ["left", "up", "down"]
+
+    b.grid = np.array([[2, 1, 1, 1],
+                       [2, 1, 1, 1],
+                       [2, 1, 1, 1],
+                       [2, 1, 1, 1]])
+
+    assert b.possible_moves() == ["right", "up", "down"]
+
+    b.grid = np.array([[2, 2, 2, 2],
+                       [1, 1, 1, 1],
+                       [1, 1, 1, 1],
+                       [1, 1, 1, 1]])
+
+    assert b.possible_moves() == ["left", "right", "down"]
+
+    b.grid = np.array([[1, 1, 1, 1],
+                       [1, 1, 1, 1],
+                       [1, 1, 1, 1],
+                       [2, 2, 2, 2]])
+
+    assert b.possible_moves() == ["left", "right", "up"]
+    b.grid = np.array([[2, 4, 2, 4],
+                       [4, 2, 4, 2],
+                       [2, 4, 2, 4],
+                       [4, 2, 4, 2]])
+
+    assert b.possible_moves() == []
+
+    # check that grid is not modified
+    b.grid = np.array([[2, 2, 2, 2],
+                       [2, 2, 2, 2],
+                       [2, 2, 2, 2],
+                       [2, 2, 2, 2]])
+
+    b.possible_moves()
+
+    assert np.all(b.grid == np.array([[2, 2, 2, 2],
+                                      [2, 2, 2, 2],
+                                      [2, 2, 2, 2],
+                                      [2, 2, 2, 2]]))
+
+def test_check_game_over():
+    b = Board(GRID_SIZE, PROB_2)
+    b.grid = np.array([[1, 1, 1, 1],
+                       [1, 1, 1, 1],
+                       [1, 1, 1, 1],
+                       [1, 1, 1, 1]])
+
+    assert b.check_game_over() == False
+
+    b.grid = np.array([[2, 1, 1, 1],
+                       [1, 4, 1, 1],
+                       [1, 1, 8, 1],
+                       [1, 1, 1, 128]])
+
+    assert b.check_game_over() == False
+
+    b.grid = np.array([[2, 2, 2, 2],
+                       [2, 2, 2, 2],
+                       [2, 2, 2, 2],
+                       [2, 2, 2, 2]])
+
+    assert b.check_game_over() == False
+
+    b.grid = np.array([[2, 2, 2, 4],
+                       [4, 2, 4, 2],
+                       [2, 4, 2, 4],
+                       [4, 2, 4, 2]])
+
+    assert b.check_game_over() == False
+
+    b.grid = np.array([[2, 4, 2, 4],
+                       [4, 2, 4, 2],
+                       [2, 4, 2, 4],
+                       [4, 2, 4, 2]])
+
+    assert b.check_game_over() == True
+
+def test_n_merges_available():
+    b = Board(GRID_SIZE, PROB_2)
+    b.grid = np.array([[1, 1, 1, 1],
+                       [1, 1, 1, 1],
+                       [1, 1, 1, 1],
+                       [1, 1, 1, 1]])
+    assert b.n_merges_available() == 0
+    b.grid = np.array([[2, 2, 1, 1],
+                       [1, 2, 1, 1],
+                       [1, 1, 2, 1],
+                       [1, 1, 1, 2]])
+    assert b.n_merges_available() == 2
+    b.grid = np.array([[2, 2, 2, 2],
+                       [2, 2, 2, 2],
+                       [2, 2, 2, 2],
+                       [2, 2, 2, 2]])
+    assert b.n_merges_available() == 16
+
+    b.grid = np.array([[2, 4, 4, 2],
+                       [4, 2, 2, 4],
+                       [4, 2, 2, 4],
+                       [2, 4, 4, 2]])
+    assert b.n_merges_available() == 8
+
+def test_smoothness():
+    b = Board(GRID_SIZE, PROB_2)
+    b.grid = np.array([[1, 1, 1, 1],
+                       [1, 1, 1, 1],
+                       [1, 1, 1, 1],
+                       [1, 1, 1, 1]])
+    assert b.smoothness() == 0
+    b.grid = np.array([[2, 2, 1, 1],
+                       [1, 2, 1, 1],
+                       [1, 1, 2, 1],
+                       [1, 1, 1, 2]])
+    assert b.smoothness() == 0
+    b.grid = np.array([[2, 2, 2, 2],
+                       [2, 2, 2, 2],
+                       [2, 2, 2, 2],
+                       [2, 2, 2, 2]])
+    assert b.smoothness() == 0
+
+    b.grid = np.array([[2, 4, 2, 1],
+                       [4, 2, 1, 1],
+                       [2, 1, 1, 1],
+                       [1, 1, 1, 1]])
+    assert b.smoothness() == 6 * np.log(2)
+
+    b.grid = np.array([[2, 4, 1, 2],
+                       [4, 1, 2, 1],
+                       [1, 2, 1, 1],
+                       [2, 1, 1, 1]])
+    assert b.smoothness() == 6 * np.log(2)
+
+    # this works, but doesn't pass due to rounding error
+    # add back in after looking into how to handle this
+    # with pytest
+    # b.grid = np.array([[128, 64, 1, 128],
+    #                    [64, 1, 128, 1],
+    #                    [1, 128, 1, 1],
+    #                    [128, 1, 1, 1]])
+    # assert b.smoothness() == 6 * np.log(2)
+
+def test_n_out_of_order():
+    b = Board(GRID_SIZE, PROB_2)
+    b.grid = np.array([[1, 1, 1, 1],
+                       [1, 1, 1, 1],
+                       [1, 1, 1, 1],
+                       [1, 1, 1, 1]])
+    assert b.n_out_of_order() == 0
+    b.grid = np.array([[2, 2, 1, 1],
+                       [1, 2, 1, 1],
+                       [1, 1, 2, 1],
+                       [1, 1, 1, 2]])
+    assert b.n_out_of_order() == 0
+    b.grid = np.array([[2, 2, 2, 2],
+                       [2, 2, 2, 2],
+                       [2, 2, 2, 2],
+                       [2, 2, 2, 2]])
+    assert b.n_out_of_order() == 0
+
+    b.grid = np.array([[2, 4, 2, 1],
+                       [4, 2, 1, 1],
+                       [2, 1, 1, 1],
+                       [1, 1, 1, 1]])
+    assert b.n_out_of_order() == 2
+
+    b.grid = np.array([[2, 4, 1, 2],
+                       [4, 1, 2, 1],
+                       [1, 2, 1, 1],
+                       [2, 1, 1, 1]])
+    assert b.n_out_of_order() == 2
+
+    b.grid = np.array([[2, 4, 8, 16],
+                       [16, 8, 4, 2],
+                       [1, 1, 1, 1],
+                       [1, 1, 1, 1]])
+    assert b.n_out_of_order() == 8
+
+    b.grid = np.array([[2, 2, 2, 2],
+                       [2, 4, 4, 2],
+                       [2, 4, 4, 2],
+                       [2, 2, 2, 2]])
+    assert b.n_out_of_order() == 8
